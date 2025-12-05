f1243c4ac80b6bcdd35254eff006c92c909d6165
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2025-12-05 18:39:00 -0300

    feat: add /about

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "hellow world"
 
+@app.about("/about")
+def about():
+    return "Api crud feita com flask"
 
 if __name__ == "__main__":
     app.run(debug=True)
